4033701ec0af95dc5e63b9979ed43c6c9c81fc27
稲葉勇人 <74127544+inabayuto@users.noreply.github.com>
2025-06-03 03:14:40 +0900

    todo実装

--- back/todo-app/app/controllers/route_main.go
+++ back/todo-app/app/controllers/route_main.go
@@ -2,7 +2,9 @@
 package controllers
 
 import (
+	"log"
 	"net/http"
+	"todo-app/app/models"
 )
 
 // top ハンドラは、ルート ("/") への HTTP リクエストを処理
@@ -11,19 +13,152 @@ func top(w http.ResponseWriter, r *http.Request) {
 	_, err := session(w, r)
 	if err != nil {
 		// generateHTML 関数を呼び出して、指定されたテンプレートを描画
-		generateHTML(w, "Hello", "layout", "public_navbar", "top")
+		generateHTML(w, nil, "layout", "public_navbar", "top")
 	} else {
-		http.Redirect(w, r, "/todos", 302)
+		http.Redirect(w, r, "/todos", http.StatusFound)
 	}
-
 }
 
+// index ハンドラは、ユーザーのTodoリストを表示する
+// セッションを確認し、認証済みであればユーザーのTodoを取得してテンプレートに渡す
 func index(w http.ResponseWriter, r *http.Request) {
+	log.Println("index handler started")
+	sess, err := session(w, r)
+	if err != nil {
+		log.Println("index handler: session error, redirecting to /:", err)
+		http.Redirect(w, r, "/", http.StatusFound)
+	} else {
+		log.Println("index handler: session found")
+		user, err := sess.GetUserBySession()
+		if err != nil {
+			log.Println("index handler: Error getting user by session:", err)
+		}
+		todos, _ := user.GetTodosByUser()
+		user.Todos = todos
+		log.Printf("index handler: User object before passing to template: %+v\n", user)
+		// generateHTML 関数を呼び出して、指定されたテンプレートを描画
+		generateHTML(w, user, "layout", "private_navbar", "index")
+	}
+}
+
+// todoNew ハンドラは、新しいTodo作成フォームを表示する
+// 認証済みであればフォームページを表示する
+func todoNew(w http.ResponseWriter, r *http.Request) {
 	_, err := session(w, r)
 	if err != nil {
-		http.Redirect(w, r, "/", 302)
+		http.Redirect(w, r, "/login", http.StatusFound)
 	} else {
 		// generateHTML 関数を呼び出して、指定されたテンプレートを描画
-		generateHTML(w, nil, "layout", "private_navbar", "index")
+		generateHTML(w, nil, "layout", "private_navbar", "todo_new")
+	}
+}
+
+// todoSave ハンドラは、新しいTodoの作成リクエストを処理する
+// フォームから内容を取得し、ユーザーに関連付けて保存後、一覧ページにリダイレクトする
+func todoSave(w http.ResponseWriter, r *http.Request) {
+	log.Println("todoSave handler started")
+	sess, err := session(w, r)
+	if err != nil {
+		log.Println("todoSave handler: session error, redirecting to /login:", err)
+		http.Redirect(w, r, "/login", http.StatusFound)
+		return
+	}
+
+	log.Println("todoSave handler: session found, parsing form.")
+	err = r.ParseForm()
+	if err != nil {
+		log.Println("todoSave handler: Form parse error:", err)
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+		return
+	}
+
+	log.Println("todoSave handler: Attempting to get user by session.")
+	user, err := sess.GetUserBySession()
+	if err != nil {
+		log.Println("todoSave handler: Error getting user by session:", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	content := r.PostFormValue("content")
+	if content == "" {
+		log.Println("todoSave handler: Content is empty.")
+		http.Error(w, "Bad Request: Todo content cannot be empty", http.StatusBadRequest)
+		return
+	}
+
+	log.Printf("todoSave handler: Creating todo for user %d with content: %s", user.ID, content)
+	if err := user.CreateTodo(content); err != nil {
+		log.Println("todoSave handler: Error creating todo:", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	log.Println("todoSave handler: Todo created successfully, redirecting to /todos.")
+	http.Redirect(w, r, "/todos", http.StatusFound)
+}
+
+// todoEdit ハンドラは、既存のTodoの編集フォームを表示する
+// URLパスからTodo IDを取得し、Todo情報を取得してテンプレートに渡す
+func todoEdit(w http.ResponseWriter, r *http.Request, id int) {
+	sess, err := session(w, r)
+	if err != nil {
+		http.Redirect(w, r, "/login", 302)
+	} else {
+		_, err := sess.GetUserBySession()
+		if err != nil {
+			log.Println(err)
+		}
+		t, err := models.GetTodo(id)
+		if err != nil {
+			log.Println(err)
+		}
+		generateHTML(w, t, "layout", "private_navbar", "todo_edit")
+	}
+}
+
+// todoUpdate ハンドラは、既存のTodoの更新リクエストを処理する
+// URLパスからTodo ID、フォームから更新内容を取得し、Todoを更新後、一覧ページにリダイレクトする
+func todoUpdate(w http.ResponseWriter, r *http.Request, id int) {
+	sess, err := session(w, r)
+	if err != nil {
+		http.Redirect(w, r, "/login", 302)
+	} else {
+		err := r.ParseForm()
+		if err != nil {
+			log.Println(err)
+		}
+		user, err := sess.GetUserBySession()
+		if err != nil {
+			log.Println(err)
+		}
+		content := r.PostFormValue("content")
+		t := &models.Todo{ID: id, Content: content, UserID: user.ID}
+		if err := t.UpdateTodo(); err != nil {
+			log.Println(err)
+		}
+		http.Redirect(w, r, "/todos", 302)
+	}
+}
+
+// todoDelete ハンドラは、既存のTodoの削除リクエストを処理する
+// URLパスからTodo IDを取得し、Todoを削除後、一覧ページにリダイレクトする
+func todoDelete(w http.ResponseWriter, r *http.Request, id int) {
+	sess, err := session(w, r)
+	if err != nil {
+		http.Redirect(w, r, "/login", http.StatusFound)
+	} else {
+		_, err := sess.GetUserBySession()
+		if err != nil {
+			log.Println(err)
+		}
+		t, err := models.GetTodo(id)
+		if err != nil {
+			log.Println(err)
+		}
+		if err := t.DeleteTodo(); err != nil {
+			log.Println(err)
+		}
+		http.Redirect(w, r, "/todos", http.StatusFound)
 	}
 }
--- back/todo-app/app/controllers/server.go
+++ back/todo-app/app/controllers/server.go
@@ -6,6 +6,8 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"regexp"
+	"strconv"
 	"todo-app/app/models"
 	"todo-app/config"
 )
@@ -18,23 +20,28 @@ func generateHTML(w http.ResponseWriter, data interface{}, filenames ...string) 
 		files = append(files, fmt.Sprintf("app/views/templates/%s.html", file))
 	}
 
+	log.Println("generateHTML: Attempting to parse template files:", files)
+
 	// テンプレートファイルをパース
 	// ParseFiles は複数のファイルを読み込み、定義されたテンプレートのセットを作成
 	templates, err := template.ParseFiles(files...)
 	if err != nil {
-		log.Printf("Template parsing error: %v", err)
+		log.Printf("generateHTML: Template parsing error: %v", err)
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
+
+	log.Println("generateHTML: Template parsing successful. Executing template with data.")
 
 	// レイアウトテンプレートを基にデータを適用し、レスポンスライターに書き出し
 	// ここで "layout" という名前のテンプレートがテンプレートセット内に存在する必要
 	err = templates.ExecuteTemplate(w, "layout", data)
 	if err != nil {
-		log.Printf("Template execution error: %v", err)
+		log.Printf("generateHTML: Template execution error: %v", err)
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
+	log.Println("generateHTML: Template executed successfully.")
 }
 
 func session(w http.ResponseWriter, r *http.Request) (sess models.Session, err error) {
@@ -46,10 +53,31 @@ func session(w http.ResponseWriter, r *http.Request) (sess models.Session, err e
 		}
 	}
 	return sess, err
-
 }
 
-// StartMainServer はアプリケーションの Web サーバーを起動
+var validPath = regexp.MustCompile("^/todos/(edit|update|delete)/([0-9]+)$")
+
+// parseURL は、URLパスからIDを抽出し、抽出したIDとHTTPレスポンスライター、リクエストオブジェクトを指定されたハンドラ関数に渡す
+// パスが正規表現に一致しない場合は 404 Not Found を返す
+func parseURL(fn func(http.ResponseWriter, *http.Request, int)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// /todos/edit/1 のようなパスを想定
+		q := validPath.FindStringSubmatch(r.URL.Path)
+		if q == nil {
+			http.NotFound(w, r)
+			return
+		}
+		id, err := strconv.Atoi(q[2])
+		if err != nil {
+			http.NotFound(w, r)
+			return
+		}
+
+		fn(w, r, id)
+	}
+}
+
+// StartMainServer はアプリケーションの Web サーバーを起動し、ルーティングを設定する
 func StartMainServer() error {
 
 	// 静的ファイルを提供するためのファイルサーバーを設定
@@ -73,6 +101,17 @@ func StartMainServer() error {
 	http.HandleFunc("/logout", logout)
 
 	http.HandleFunc("/todos", index)
+
+	http.HandleFunc("/todos/new", todoNew)
+
+	http.HandleFunc("/todos/save", todoSave)
+
+	// IDを含む /todos/update/{id} 形式のパスを parseURL 経由で todoUpdate ハンドラにルーティング
+	http.HandleFunc("/todos/update", parseURL(todoUpdate))
+	// IDを含む /todos/edit/{id} 形式のパスを parseURL 経由で todoEdit ハンドラにルーティング
+	http.HandleFunc("/todos/edit", parseURL(todoEdit))
+	// IDを含む /todos/delete/{id} 形式のパスを parseURL 経由で todoDelete ハンドラにルーティング
+	http.HandleFunc("/todos/delete", parseURL(todoDelete))
 
 	// 指定されたポートで HTTP リクエストのリスニングを開始する
 	log.Printf("Starting server on port %s...", config.Config.Port) // サーバー起動ログを追加
